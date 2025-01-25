0c833c89e1d3dd15b44740073b1c1112cd248269
Ishaan Kor <ishaankor@gmail.com>
2025-01-24 17:06:39 -0800

    Modified and incorporated JS to add listeners

---
+++ global.js
@@ -0,0 +1,79 @@
+console.log('IT’S ALIVE!');
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'https://github.com/ishaankor', title: 'Profile' },
+    { url: 'resume/', title: 'Resume' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+    }  
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname,
+    );
+    if (a.host !== location.host) {
+        a.toggleAttribute('target', '_blank')
+    }
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+        <label class="color-scheme">
+            Theme:
+            <select>
+                <option value="light dark"> Automatic </option>
+                <option value="light"> Light </option>
+                <option value="dark"> Dark </option>
+            </select>
+        </label>`
+);
+
+let select = document.querySelector('.color-scheme select');
+
+if ("colorScheme" in localStorage) {
+    let theme = localStorage.colorScheme;
+    document.documentElement.style.colorScheme = theme;
+    select.value = theme; 
+}
+
+select.addEventListener('input', function (event) {
+    let selectedTheme = event.target.value;
+    localStorage.colorScheme = selectedTheme; 
+    console.log('Color scheme changed to', selectedTheme);
+    document.documentElement.style.colorScheme = selectedTheme;
+});
+
+let contact_form = document.querySelector('.contact-form')
+  
+contact_form?.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let data = new FormData(contact_form)
+    let url = event.target.action + "?";
+    let params = [];
+    for (let [name, value] of data) {
+        params.push(encodeURIComponent(name) + "=" + encodeURIComponent(value));
+    }
+    url += params.join("&"); 
+    location.href = url;
+});
