efac3d3447f9704e3b332f057d1bdf053eec0add
Ishaan Kor <ishaankor@gmail.com>
2025-01-31 18:32:19 -0800

    TEsting JSON implementation

--- global.js
+++ global.js
@@ -74,3 +74,48 @@ contact_form?.addEventListener('submit', function (event) {
     url += params.join("&"); 
     location.href = url;
 });
+
+let url = '/lib/projects.json';
+console.log(fetchJSON(url));
+
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        } else {
+            console.log(response)
+            const data = await response.json();
+            return data; 
+        }
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+
+// export function renderProjects(project, containerElement) {
+//     // Your code will go here
+    // containerElement.innerHTML = '';
+    // const article = document.createElement('article');
+    // article.innerHTML = `
+    //     <h3>${project.title}</h3>
+    //     <img src="${project.image}" alt="${project.title}">
+    //     <p>${project.description}</p>
+    // `;
+    // containerElement.appendChild(article);
+// }
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    // write javascript that will allow dynamic heading levels based on previous function
+    containerElement.innerHTML = '';
+    const article = document.createElement('article');
+    article.innerHTML = `
+        <${headingLevel}>${project.title}</${headingLevel}>
+        <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+}
+
+
