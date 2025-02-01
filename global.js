4109cbaf673964398c91f52b86a55fa34077b418
Ishaan Kor <ishaankor@gmail.com>
2025-01-31 20:40:26 -0800

    Fixed typos

--- global.js
+++ global.js
@@ -34,10 +34,10 @@ function $$(selector, context = document) {
 
 let pages = [
     { url: '/my-data-science-portfolio/index.html', title: 'Home' },
-    { url: '/my-data-science-portfolio//projects/index.html', title: 'Projects' },
-    { url: '/my-data-science-portfolio//resume/index.html', title: 'Resume' },
+    { url: '/my-data-science-portfolio/projects/index.html', title: 'Projects' },
+    { url: '/my-data-science-portfolio/resume/index.html', title: 'Resume' },
     { url: 'https://github.com/ishaankor', title: 'Profile' },
-    { url: '/my-data-science-portfolio//contact/index.html', title: 'Contact' }
+    { url: '/my-data-science-portfolio/contact/index.html', title: 'Contact' }
 ];
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
